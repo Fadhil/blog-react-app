ccc1ee9f67e6edf7d3e37d011b3e770ca6b40e62
Fadhil Luqman <fadhil.luqman@gmail.com>
2025-03-12 09:48:23 +0800

    test auto rollout

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>This is a test of Argo CD again</p>
         <p>Is this even working?</p>
         <p>It was working</p>
+        <p>Now let's see if it does a rollout automatically</p>
       </header>
     </div>
   );
