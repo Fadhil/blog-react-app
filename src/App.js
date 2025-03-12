986c608b1c3813f5edc57f87df8f57d2d0049ac6
Fadhil Luqman <fadhil.luqman@gmail.com>
2025-03-12 13:38:14 +0800

    auto rollout please?

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ function App() {
       <header className="App-header">
         <h2>Hello World!</h2>
         <p>This is a test of Argo CD again</p>
-        <p>Is this even working?</p>
-        <p>It was working</p>
         <p>Now let's see if it does a rollout automatically</p>
       </header>
     </div>
